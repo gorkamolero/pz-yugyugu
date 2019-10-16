daacdd70e4639348df4b260aaec3a1a2c5b2d059
Gorka <gorka.molero@gmail.com>
2019-10-16 17:51:45 +0200

    Adding Vuetify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vuebar from 'vuebar'
+import vuetify from './plugins/vuetify';
 Vue.use(Vuebar)
 
 Vue.config.productionTip = false
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#pz-email-builder')
